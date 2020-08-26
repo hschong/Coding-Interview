03516bbd4f7fcc7c3c3e5c0543d3ec3c15659ccb
HEESEOK <heeseok.chong@gmail.com>
2020-08-26 17:25:10 +0900

    Added list.sort() and sorted()

---
+++ basic_examples.py
@@ -0,0 +1,10 @@
+integers = [1, 2, 5, 4, 7, 3]
+characters = ['a', 'c', 'd', 'k', 'i', 'j']
+
+# Sort the items of the list in place.
+integers.sort()
+integers.sort(reverse=True)
+
+# Return a new sorted list from the items in iterable.
+sorted(characters)
+sorted(characters, reverse=True)
