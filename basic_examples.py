f5ed32f5328fbedb04947cb4b650a3f2e53b2438
HEESEOK <heeseok.chong@gmail.com>
2020-08-26 22:32:24 +0900

    Added list methods

--- basic_examples.py
+++ basic_examples.py
@@ -1,5 +1,7 @@
 integers = [1, 2, 5, 4, 7, 3]
 characters = ['u', 'a', 'e', 'c', 'k', 'i']
+duplicates = ['a', 'b', 'a', 'c', 'c']
+# invalid_list = [a, b, c, d, e]
 string = 'apple#banana#cherry#orange'
 
 
@@ -13,11 +15,46 @@ sorted_characters = sorted(characters, reverse=True)
 
 # Split a string into lists
 # Setting the maxsplit parameter to 3, will return a list with 4
-lists = string.split('#', 3)
+new_list = string.split('#', 3)
 
 # Convert a string to a list
 new_list = list(string)
-new_list = [character for character in string]
 
 # Convert a list to a string
 new_string = ''.join(new_list)
+
+# Reverse a list
+integers.reverse()
+# reversed() returns a reverse iterator
+new_integers = list(reversed(integers))
+
+# Remove duplicates from a list
+new_list = list(dict.fromkeys(duplicates))
+
+# Append/Insert an object in a list
+new_list.append(1)
+new_list.insert(0, 1)
+new_list.remove(1)
+
+# Remove and return item at index (default last)
+last_item = new_list.pop()
+item_at_1 = new_list.pop(1)
+
+# Return first index of value from list
+if 'a' not in new_list:
+    print('a is not in the list.')
+else:
+    index = new_list.index('a')
+
+# Remove all items from list
+new_list.clear()
+del new_list[:]
+new_list = []
+new_list[:] = []
+new_list *= 0
+
+# new_list = new_list[:1] + new_list[1:]
+new_list[1:] = []
+
+for idx, val in enumerate(new_list):
+    print(idx, val)
