10faa4c15e04ade7bdadc53c4d92770c444ad574
HEESEOK <heeseok.chong@gmail.com>
2020-08-28 17:55:38 +0900

    added str.replace()

--- basic_examples.py
+++ basic_examples.py
@@ -11,6 +11,8 @@ duplicates = ['a', 'b', 'a', 'c', 'c']
 string = 'apple#banana#cherry#orange'
 languages = ['Korean']
 languages_tuple = ('English', 'German')
+name = 'heeseok'
+
 
 # Sort the items of the list in place.
 integers.sort()
@@ -20,40 +22,46 @@ integers.sort(reverse=True)
 sorted_characters = sorted(characters)
 sorted_characters = sorted(characters, reverse=True)
 
-# Split a string into lists
-# Setting the maxsplit parameter to 3, will return a list with 4
+
+# Split a string into lists.
+# Setting the maxsplit parameter to 3, will return a list with 4.
 new_list = string.split('#', 3)
 
-# Convert a string to a list
+# Convert a string to a list.
 new_list = list(string)
 
-# Convert a list to a string
+# Convert a list to a string.
 new_string = ''.join(new_list)
 
-# Reverse a list
+# string object is immutable.
+# name[0] = 'H' wrong! using str.replace() instead of assignment.
+new_name = name.replace('h', 'H')
+
+
+# Reverse a list.
 integers.reverse()
-# reversed() returns a reverse iterator
+# reversed() returns a reverse iterator.
 new_integers = list(reversed(integers))
 
-# Remove duplicates from a list
+# Remove duplicates from a list.
 new_list = list(dict.fromkeys(duplicates))
 
-# Append/Insert an object in a list
+# Append/Insert an object in a list.
 new_list.append(1)
 new_list.insert(0, 1)
 new_list.remove(1)
 
-# Remove and return item at index (default last)
+# Remove and return item at index (default last).
 last_item = new_list.pop()
 item_at_1 = new_list.pop(1)
 
-# Return first index of value from list
+# Return first index of value from list.
 if 'a' not in new_list:
     print('a is not in the list.')
 else:
     index = new_list.index('a')
 
-# Remove all items from list
+# Remove all items from list.
 new_list.clear()
 del new_list[:]
 new_list = []
@@ -66,8 +74,13 @@ new_list[1:] = []
 occurrences = new_list.count('a')
 languages.extend(languages_tuple)
 
+
+# For loop
 for idx, val in enumerate(new_list):
     print(idx, val)
+
+for countdown in 5, 4, 3, 2, 1, 'hey!':
+    print(countdown)
 
 
 # Lambda
@@ -75,8 +88,11 @@ def calculate(a, b, func):
     return func(a, b)
 
 
-# add = lambda a, b: a+b
+# add = lambda a, b: a + b
 def add(a, b): return a+b
+
+
+calculate(3, 4, add)
 
 # judge = lambda score: 'pass' if score>=80 else 'fail'
 def judge(score): return 'pass' if score >= 80 else 'fail'
