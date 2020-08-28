1efbf37d448e472f24ec8a4d85f45dbd20869bc6
HEESEOK <heeseok.chong@gmail.com>
2020-08-28 19:22:10 +0900

    initial commit

---
+++ string_examples.py
@@ -0,0 +1,17 @@
+string = 'apple#banana#cherry#orange'
+name = 'heeseok'
+
+
+# Split a string into lists.
+# Setting the maxsplit parameter to 3, will return a list with 4.
+new_list = string.split('#', 3)
+
+# Convert a string to a list.
+new_list = list(string)
+
+# Convert a list to a string.
+new_string = ''.join(new_list)
+
+# string object is immutable.
+# name[0] = 'H' wrong! using str.replace() instead of assignment.
+new_name = name.replace('h', 'H')
