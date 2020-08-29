10b9267dc113d8a1324ca687ac1df1133a084d3a
HEESEOK <heeseok.chong@gmail.com>
2020-08-30 04:12:37 +0900

    + typing List

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,8 @@
 # 125. Valid palindrome
 # https://leetcode.com/problems/valid-palindrome/#
 
+from typing import List
+
 palindrome = 'A man, a plan, a canal: Panama'
 palindrome_1 = 'aba'
 palindrome_2 = 'abba'
@@ -10,21 +12,19 @@ test_sample = palindrome
 
 
 class Solution:
-    def remove_non_alphanumeric_characters(self, S: str) -> str:
-        alphanumeric_characters = [char for char in S if char.isalnum()]
-        return ''.join(alphanumeric_characters)
+    def remove_non_alphanumeric_characters(self, S: str) -> List:
+        return [char.lower() for char in S if char.isalnum()]
 
-    def is_palindrome(self, S: str) -> bool:
-        if len(S) <= 1:
+    def is_palindrome(self, lst: List) -> bool:
+        if len(lst) <= 1:
             return True
-        elif S[0] == S[-1]:
-            return self.is_palindrome(S[1:-1])
+        elif lst[0] == lst[-1]:
+            return self.is_palindrome(lst[1:-1])
         else:
             return False
 
     def isPalindrome(self, S: str) -> bool:
-        alphanumeric_characters = self.remove_non_alphanumeric_characters(S)
-        return self.is_palindrome(alphanumeric_characters.lower())
+        return self.is_palindrome(self.remove_non_alphanumeric_characters(S))
 
 
 sol = Solution()
