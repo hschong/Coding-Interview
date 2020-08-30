c5cb872cb710ae31d1f75ff99c388f612e44936a
HEESEOK <heeseok.chong@gmail.com>
2020-08-30 23:56:14 +0900

    # 344. Reverse String

---
+++ reverse_string.py
@@ -0,0 +1,20 @@
+# 344. Reverse String
+# https://leetcode.com/problems/reverse-string/
+
+from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s)-1
+
+        while right > left:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+    def reverse_string(self, L: List(str)) -> None:
+        L.reverse()
