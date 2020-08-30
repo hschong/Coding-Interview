7e7ba3b7c0c5d134589e9031e76928f56593e34f
HEESEOK <heeseok.chong@gmail.com>
2020-08-30 23:04:07 +0900

    + deque.popleft() -> O(1)

--- palindrome.py
+++ palindrome.py
@@ -1,14 +1,9 @@
 # 125. Valid palindrome
 # https://leetcode.com/problems/valid-palindrome/#
 
-from typing import List
+import collections
 
 palindrome = 'A man, a plan, a canal: Panama'
-palindrome_1 = 'aba'
-palindrome_2 = 'abba'
-non_palindrome_1 = 'abdcba'
-non_palindrome_2 = 'bbbba'
-test_sample = palindrome
 
 
 def remove_non_alphanumeric_characters(S: str) -> str:
@@ -22,12 +17,27 @@ def is_palindrome(S: str) -> bool:
 
 
 class Solution:
+    # Use List: pop([i]) -> O(n)
     def isPalindrome(self, S: str) -> bool:
         alphanumeric_characters = [char.lower()
                                    for char in S if char.isalnum()]
 
         while len(alphanumeric_characters) > 1:
             if alphanumeric_characters.pop(0) != alphanumeric_characters.pop():
+                return False
+
+        return True
+
+    # Use deque: list-like container with fast appends and pops on either end. popleft() -> O(1)
+    def is_palindrome_1(self, S: str) -> bool:
+        alphanumeric_characters: Deque = collections.deque()
+
+        for char in S:
+            if char.isalnum():
+                alphanumeric_characters.append(char.lower())
+
+        while len(alphanumeric_characters) > 1:
+            if alphanumeric_characters.popleft() != alphanumeric_characters.pop():
                 return False
 
         return True
