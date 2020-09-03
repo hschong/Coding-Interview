cff55a07b07a86d11a89cbcd066d555f206b5fe1
HEESEOK <heeseok.chong@gmail.com>
2020-09-04 00:32:52 +0900

    improved by using 'if in'

--- E 1. Two Sum.py
+++ E 1. Two Sum.py
@@ -3,12 +3,6 @@
 
 # Easy
 
-# Given an array of integers nums and and integer target, return the indices of the two numbers such that they add up to target.
-
-# You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-# You can return the answer in any order.
-
 
 # Example 1:
 # Input: nums = [2,7,11,15], target = 9
@@ -39,6 +33,14 @@ class Solution:
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
+    def get_two_sum(self, nums: List[int], target: int) -> List[int]:
+        for idx, num in enumerate(nums):
+            complement = target - num
+
+            # 'if in' is much faster than 'for in range'.
+            if complement in nums[idx+1:]:
+                return [idx, idx+1+nums[idx+1:].index(complement)]
+
 
 sol = Solution()
-print(sol.twoSum([3, 2, 4], 6))
+print(sol.get_two_sum([3, 2, 4], 6))
