8dd3cb19faa9536ecc2732f6242a304d30ffb3f8
HEESEOK <heeseok.chong@gmail.com>
2020-09-06 19:00:51 +0900

    defaultdict.values() returns  <class 'dict_values'> not <class 'list'>

--- M 49. Group Anagrams.py
+++ M 49. Group Anagrams.py
@@ -4,16 +4,16 @@
 # Medium
 
 from typing import List
-import collections
+from collections import defaultdict
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         # 1. put sorted word and org word into a dictionary
         # 2. count
-        anagrams_dic = collections.defaultdict(list)
+        anagrams_dic = defaultdict(list)
         for word in strs:
             # defaultdict[key].append(value) -> append value into list of values of key
             anagrams_dic[''.join(sorted(word))].append(word)
 
-        return anagrams_dic.values()
+        return list(anagrams_dic.values())
