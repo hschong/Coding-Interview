ad1040189a319942ccbc0c0b56a10aa5c395947b
HEESEOK <heeseok.chong@gmail.com>
2020-09-09 00:23:38 +0900

    made codes simple

--- 0024M. Swap Nodes in Pairs.py
+++ 0024M. Swap Nodes in Pairs.py
@@ -17,7 +17,6 @@ class ListNode:
 
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
-        # 1. odd or even elements ?
         if not head:
             return head
 
@@ -33,168 +32,56 @@ class Solution:
 
                 # initialize and swap
                 next_node = node_b.next
-                node_a, node_b = node_b, node_a
-                # node_a -> node_b
-                node_a.next = node_b
-                # node_b.next -> next
-                node_b.next = next_node
-                new_head = node_a
-
-                print("1. prev_node is None")
-                print("prev_node = ", prev_node)
-                print("node_a = ", node_a)
-                print("node_b = ", node_b)
-                print("next_node = ", next_node)
-                print("")
+                node_b.next = node_a
+                node_a.next = next_node
+                new_head = node_b
             else:
                 if node_b is not None:
-                    # prev_node, node_a and node_b are not None.
+                    # node_a and node_b are not None.
                     next_node = node_b.next
-                    node_a, node_b = node_b, node_a
-                    # prev_node -> node_a -> node_b -> next
-                    prev_node.next = node_a
-                    node_a.next = node_b
-                    node_b.next = next_node
-
-                    print("2. prev_node is not None")
-                    print("prev_node = ", prev_node)
-                    print("node_a = ", node_a)
-                    print("node_b = ", node_b)
-                    print("next_node = ", next_node)
-                    print("")
+                    node_b.next = node_a
+                    node_a.next = next_node
+                    prev_node.next = node_b
                 else:
-                    # node_b is None: the linked list has odd elements.
+                    # odd elements
                     prev_node.next = node_a
                     node_a.next = None
-
-                    print("3. node_b is None")
-                    print("prev_node = ", prev_node)
-                    print("node_a = ", node_a)
-                    print("node_b = ", node_b)
-                    print("next_node = ", next_node)
-                    print("")
 
                     return new_head
 
             # 2 steps forward
-            # node_b is not None
             if next_node is None:
                 return new_head
 
-            prev_node = node_b
+            prev_node = node_a
             node_a = next_node
             node_b = next_node.next
-
-            print("4. move forward")
-            print("prev_node = ", prev_node)
-            print("node_a = ", node_a)
-            print("node_b = ", node_b)
-            print("next_node = ", next_node)
-            print("")
 
         return new_head
 
 
-# [1,2,3,4]
+def swap_pairs(head: ListNode) -> ListNode:
+    # prev_node -> node_a -> node_b -> next_node
+    if not head:
+        return head
 
-# 1. prev_node is None
-# prev_node =  None
-# node_a =  ListNode{val: 2, next: ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_b =  ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# next_node =  ListNode{val: 3, next: ListNode{val: 4, next: None}}
+    new_head = ListNode()
+    node_a, node_b = head, head.next
+    prev_node = new_head
+    prev_node.next = node_a
+    next_node = None
 
-# 4. move forward
-# prev_node =  ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# node_a =  ListNode{val: 2, next: ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_b =  ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# next_node =  ListNode{val: 3, next: ListNode{val: 4, next: None}}
+    while node_a and node_b:
+        next_node = node_b.next
+        node_b.next = node_a  # b -> a
+        node_a.next = next_node  # a -> next
+        prev_node.next = node_b  # prev -> b
 
-# 2. prev_node is not None
-# prev_node =  ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_a =  ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_b =  ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# next_node =  ListNode{val: 3, next: ListNode{val: 4, next: None}}
+        if next_node is None:
+            break
 
-# 4. move forward
-# prev_node =  ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# node_a =  ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_b =  ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# next_node =  ListNode{val: 3, next: ListNode{val: 4, next: None}}
+        prev_node = node_a
+        node_a = next_node
+        node_b = next_node.next
 
-# 2. prev_node is not None
-# prev_node =  ListNode{val: 2, next: ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_a =  ListNode{val: 2, next: ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_b =  ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# next_node =  ListNode{val: 3, next: ListNode{val: 4, next: None}}
-
-# 4. move forward
-# prev_node =  ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# node_a =  ListNode{val: 2, next: ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_b =  ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# next_node =  ListNode{val: 3, next: ListNode{val: 4, next: None}}
-
-# 2. prev_node is not None
-# prev_node =  ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_a =  ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_b =  ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# next_node =  ListNode{val: 3, next: ListNode{val: 4, next: None}}
-
-# 4. move forward
-# prev_node =  ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# node_a =  ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_b =  ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# next_node =  ListNode{val: 3, next: ListNode{val: 4, next: None}}
-
-# 2. prev_node is not None
-# prev_node =  ListNode{val: 2, next: ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_a =  ListNode{val: 2, next: ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_b =  ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# next_node =  ListNode{val: 3, next: ListNode{val: 4, next: None}}
-
-# 4. move forward
-# prev_node =  ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# node_a =  ListNode{val: 2, next: ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_b =  ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# next_node =  ListNode{val: 3, next: ListNode{val: 4, next: None}}
-
-# 2. prev_node is not None
-# prev_node =  ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_a =  ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_b =  ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# next_node =  ListNode{val: 3, next: ListNode{val: 4, next: None}}
-
-# 4. move forward
-# prev_node =  ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# node_a =  ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_b =  ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# next_node =  ListNode{val: 3, next: ListNode{val: 4, next: None}}
-
-# 2. prev_node is not None
-# prev_node =  ListNode{val: 2, next: ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_a =  ListNode{val: 2, next: ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_b =  ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# next_node =  ListNode{val: 3, next: ListNode{val: 4, next: None}}
-
-# 4. move forward
-# prev_node =  ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# node_a =  ListNode{val: 2, next: ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_b =  ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# next_node =  ListNode{val: 3, next: ListNode{val: 4, next: None}}
-
-# 2. prev_node is not None
-# prev_node =  ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_a =  ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_b =  ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# next_node =  ListNode{val: 3, next: ListNode{val: 4, next: None}}
-
-# 4. move forward
-# prev_node =  ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# node_a =  ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_b =  ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# next_node =  ListNode{val: 3, next: ListNode{val: 4, next: None}}
-
-# 2. prev_node is not None
-# prev_node =  ListNode{val: 2, next: ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_a =  ListNode{val: 2, next: ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
-# node_b =  ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
-# ... 81082 more lines
+    return new_head.next
