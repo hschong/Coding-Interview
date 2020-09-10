13574cb4c70961c4036f2a41eb5d15d550e422c3
HEESEOK <heeseok.chong@gmail.com>
2020-09-10 18:13:01 +0900

    + 739. Daily Temperatures

---
+++ 0739M. Daily Temperatures.py
@@ -0,0 +1,32 @@
+# 739. Daily Temperatures
+# https://leetcode.com/problems/daily-temperatures/
+
+# Given a list of daily temperatures T, return a list such that, for each day in the input, tells you how many days you would have to wait until a warmer temperature. If there is no future day for which this is possible, put 0 instead.
+
+# For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+
+# Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
+
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        stack = []  # for index
+        result = [0] * len(T)
+
+        for i, cur in enumerate(T):
+            while stack and cur > T[stack[-1]]:
+                last = stack.pop()
+                result[last] = i - last
+
+            stack.append(i)
+
+        return result
+
+
+T = [73, 74, 75, 71, 69, 72, 76, 73]
+output = [1, 1, 4, 2, 1, 1, 0, 0]
+
+sol = Solution()
+print(sol.dailyTemperatures(T))
