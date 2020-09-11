8387c9ac1a4d8037e9630a91bb76fb298842184d
HEESEOK <heeseok.chong@gmail.com>
2020-09-11 21:32:35 +0900

    + slicing string

--- 0125E. valid palindrome.py
+++ 0125E. valid palindrome.py
@@ -1,27 +1,12 @@
 # 125. Valid palindrome
 # https://leetcode.com/problems/valid-palindrome/#
 
-# Easy
-
 from collections import deque
 from typing import Deque
 
 
-palindrome = 'A man, a plan, a canal: Panama'
-
-
-def remove_non_alphanum_chars(S: str) -> str:
-    return ''.join([char.lower() for char in S if char.isalnum()])
-
-
-def is_palindrome(S: str) -> bool:
-    return True if S == S[::-1] else False
-    # Do not use recursive call using list slicing because
-    # copy overhead occurred like list.copy(), list() and [:]
-
-
 class Solution:
-    # Use List: pop([i]) -> O(n)
+    # Use List: pop([i]) -> O(n), 280ms
     def isPalindrome(self, S: str) -> bool:
         alphanum_chars = [char.lower() for char in S if char.isalnum()]
 
@@ -31,8 +16,8 @@ class Solution:
 
         return True
 
-    # Use deque: list-like container with fast appends and pops on either end. popleft() -> O(1)
-    def is_palindrome_1(self, S: str) -> bool:
+    # Use deque: list-like container with fast appends and pops on either end. popleft() -> O(1), 52ms
+    def is_palindrome(self, S: str) -> bool:
         alphanum_chars: Deque = deque()
 
         for char in S:
@@ -44,3 +29,24 @@ class Solution:
                 return False
 
         return True
+
+
+palindrome = 'A man, a plan, a canal: Panama'
+
+
+def is_palindrome(S: str) -> bool:  # 36ms
+    alpha_only = ''.join([char.lower() for char in S if char.isalnum()])
+    return True if alpha_only == alpha_only[::-1] else False
+    # Do not use recursive call using list slicing because
+    # copy overhead occurred like list.copy(), list() and [:]
+
+
+def is_palindrome_1(S: str) -> bool:
+    # stack overflow occurred due to limited recursion depth(1000)
+    # use generator instead of recursion
+    if len(S) == 0:
+        return True
+
+    alpha_only = ''.join([char.lower() for char in S if char.isalnum()])
+
+    return is_palindrome_1(alpha_only[1:-1]) if alpha_only[0] == alpha_only[-1] else False
