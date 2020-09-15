f420875168cb79d058a80f03439eca47a70ae2bb
HEESEOK <heeseok.chong@gmail.com>
2020-09-15 15:44:06 +0900

    + storage class    global and nonlocal

--- basic_examples.py
+++ basic_examples.py
@@ -81,3 +81,29 @@ lst_c[0].append(4)
 
 # lst_d == [[1, 2, 3, 4], 'a', 'b', 'c']
 # lst_e == [[1, 2, 3], 'a', 'b', 'c']
+
+
+# Storage class  https://www.geeksforgeeks.org/storage-classes-in-c-with-examples/
+
+def manipulate_global_variable_in_func():
+    global x
+    x = 11
+
+
+def func():
+    x = 12
+    print("local x = ", x)
+
+    def func_in_func():
+        nonlocal x
+        x = 13
+
+    func_in_func()
+    print("after func_in_func() = ", x)
+
+
+x = 10
+print("golbal x = ", x)
+manipulate_global_variable_in_func()
+print("after manipulate_global_variable_in_func() = ", x)
+func()
