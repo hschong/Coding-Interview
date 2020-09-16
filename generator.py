9465f5a8404e2ff3865e893621bc08d25144beb1
HEESEOK <heeseok.chong@gmail.com>
2020-09-16 21:50:44 +0900

    + iterator

--- generator.py
+++
@@ -1,7 +0,0 @@
-# iterator
-it = range(10).__iter__()
-
-num = it.__next__()
-print(num)
-num = it.__next__()
-print(num)
---
+++ iterator.py
@@ -0,0 +1,34 @@
+import random
+
+
+# iterator
+it = range(10).__iter__()
+
+num = it.__next__()
+print(num)
+num = it.__next__()
+print(num)
+
+
+# iter(object[, sentinel])
+for num in iter(lambda: random.randrange(0, 100), 7):
+    print("random number is ", num)
+
+
+# range()
+class Counter():
+    def __init__(self, stop):
+        self.cur = 0
+        self.stop = stop
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.cur < self.stop:
+            result = self.cur
+            self.cur += 1
+
+            return result
+        else:
+            raise StopIteration
