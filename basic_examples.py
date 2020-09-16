364cc5b52b6acfa5f78a6e8778b7679d16ad6579
HEESEOK <heeseok.chong@gmail.com>
2020-09-16 20:40:43 +0900

    storage class

--- basic_examples.py
+++ basic_examples.py
@@ -47,32 +47,6 @@ print(f'{idx}: {fruit}')  # 3.6+
 pprint.pprint(locals())
 
 
-# Storage class
-# https://www.geeksforgeeks.org/storage-classes-in-c-with-examples/
-def manipulate_global_variable_in_func():
-    global x
-    x = 11
-
-
-def func():
-    x = 12
-    print("local x = ", x)
-
-    def func_in_func():
-        nonlocal x
-        x = 13
-
-    func_in_func()
-    print("after func_in_func() = ", x)
-
-
-x = 10
-print("golbal x = ", x)
-manipulate_global_variable_in_func()
-print("after manipulate_global_variable_in_func() = ", x)
-func()
-
-
 # Differences between 'is' and  '=='
 # Return a shallow copy of the list.
 lst_a = [1, 2, 3]
